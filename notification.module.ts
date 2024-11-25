f86464b7cc9d9c7d9b5271c3fb13f9941dd6144e
worldzhy <worldzhy@126.com>
2024-11-26 05:31:55 +0800

    refactor: delete webhook module

--- notification.module.ts
+++ notification.module.ts
@@ -2,15 +2,9 @@ import {Global, Module} from '@nestjs/common';
 import {EmailModule} from './email/email.module';
 import {SmsModule} from './sms/sms.module';
 import {TraceableEmailModule} from './traceable/tracaable-email.module';
-import {NotificationWebhookModule} from './webhook/webhook.module';
 
 @Global()
 @Module({
-  imports: [
-    EmailModule,
-    SmsModule,
-    TraceableEmailModule,
-    NotificationWebhookModule,
-  ],
+  imports: [EmailModule, SmsModule, TraceableEmailModule],
 })
 export class NotificationModule {}
